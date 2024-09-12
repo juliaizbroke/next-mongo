ef4d841903f1ba09b8250b5aac30e8b2186e30e5
Thet Myat Noe Thwin <u6530213@au.edu>
2024-09-12 14:27:00 +0700

    fix env

--- app/category/page.js
+++ app/category/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const { register, handleSubmit } = useForm();
 
   async function fetchCategory() {
-    const data = await fetch("http://localhost:3000/api/category");
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/category`);
     const c = await data.json();
     setCategoryList(c);
   }
@@ -18,7 +18,7 @@ export default function Home() {
   }, []);
 
   function createCategory(data) {
-    fetch("http://localhost:3000/api/category", {
+    fetch(`${process.env.NEXT_PUBLIC_API_BASE}/category`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
